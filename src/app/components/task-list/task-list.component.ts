1fc7c9a31f6d23011bf91af8c88e7ae9fd57ac5a
seregamazur <serhiy.mazur0@gmail.com>
2019-06-16 16:57:38 +0300

    Added calendar picker to task list

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { TaskService } from 'src/app/services/task.service';
 import { ActivatedRoute } from "@angular/router";
 import { Router } from '@angular/router';
 import {  OrganizationService } from 'src/app/services/organization.service';
+import { DateRange } from '@uiowa/date-range-picker';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.scss'],
 })
-export class TaskListComponent implements OnInit {
 
+export class TaskListComponent implements OnInit {
+  dateRange: DateRange;
   tasks: Object;
   org: Object;
 
@@ -18,6 +20,7 @@ export class TaskListComponent implements OnInit {
     private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.dateRange =  new DateRange(new Date());
     this.taskService.findTasksByOrganizationId(this.route.snapshot.paramMap.get("id"))
     .subscribe(data => {
         this.tasks = data;
@@ -29,15 +32,23 @@ export class TaskListComponent implements OnInit {
       console.log(this.org)
     });
   }
-  refOnComments(comId){
+  refOnComments(comId: BigInteger){
     this.router.navigateByUrl('/home/comments/'+ comId);
   }
-  handleDelete(id){
+  handleDelete(id: BigInteger){
     this.taskService.deleteTask(id);
   }
 
-  handleEdit(id){
+  handleEdit(id: BigInteger){
     this.router.navigateByUrl('/home/task/edit/' + id);
+  }
+
+  changeDate(dateRange: DateRange) {
+    this.dateRange = dateRange;
+    this.taskService.findTasksByDate(this.dateRange).subscribe(data => {
+      this.tasks = data;
+      console.log(this.tasks);
+  });
   }
 
 }
